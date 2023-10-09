71fc27538a8b4a45bdc45452e3b107087f03e4bd
john makateto <jmakateto@gmail.com>
2023-10-09 23:51:01 +0300

    made changes

--- server/app.py
+++ server/app.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -17,11 +14,31 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return jsonify([plant.serialize() for plant in plants])
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, plant_id):
+        plant = Plant.query.get(plant_id)
+        if plant:
+            return jsonify(plant.serialize())
+        else:
+            return jsonify({"message": "Plant not found"}), 404
+
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name=data['name'],
+            image=data['image'],
+            price=data['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
+        return jsonify(new_plant.serialize()), 201
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:plant_id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
